b653a9e705a449e027b262f3e3f8957b4b4a61c1
Max Gaukler <development@maxgaukler.de>
2017-06-15 18:58:58 +0200

    Proof of concept fertig. Muss nur noch eingebaut werden.

---
+++ extra.js
@@ -0,0 +1,8 @@
+
+jQuery(document).ready(function($) {
+  $("#header-site-info").replaceWith('<div id="archiv-warning">Dies ist das Archiv der alten Webseite des FAU FabLab.<br/>' +
+  '<a href="https://fablab.fau.de/">▶ Zur neuen Webseite ◀</a><br/>' +
+  'Bitte melde kaputte Links an <a href="mailto:website@fablab.fau.de">website@fablab.fau.de</a>.' + 
+  '<div class="english">You are viewing the archived, outdated version of our website. <a href="https://fablab.fau.de/">Go to current website</a></div>' +
+  '</div>');
+});
--- make.sh
+++ make.sh
@@ -3,7 +3,23 @@ set -e
 cd "$(dirname "$0")"
 mkdir -p archiv-raw
 cd archiv-raw
-httrack --update https://fablab.fau.de/ '-wiki*'
+# HTTrack mit ein paar Filtern:
+# Der Filter '-*"*' ist gegen einen Bug in httrack, das dann immer https://fablab.fau.de/bla/"https://fablab.fau.de/foo" ... laden will.
+# httrack --continue --update https://fablab.fau.de/ '-fablab.fau.de/wiki/*' '-fablab.fau.de/app/*' '-fablab.fau.de/user/*'  '-fablab.fau.de/comment/reply/*'  '-*"*' '-mime:application/rss+xml'
 cd ..
-# TODO replace CSS, remove termine, ...
-# or just throw this completely out of the google search index
+
+# TODO robots.txt
+
+# copy over to archiv-filtered/
+chmod u+rX -R archiv-raw/
+test -d archiv-filtered && rm -r archiv-filtered/
+cp -r --reflink=auto archiv-raw/ archiv-filtered/
+
+# filter: add extra CSS and JS
+for cssfile in archiv-filtered/fablab.fau.de/sites/all/themes/fusion/fusion_core/css/grid16-fluid*.css; do
+    cat extra.css >> $cssfile
+done
+
+for jsfile in archiv-filtered/fablab.fau.de/sites/fablab.fau.de/files/js/js_*.js; do
+    cat extra.js >> $jsfile
+done
